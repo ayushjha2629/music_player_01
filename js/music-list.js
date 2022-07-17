d2835db26624de97488530eda32d6a0cdb55814e
ayushjha2629 <108911967+ayushjha2629@users.noreply.github.com>
2022-07-17 13:16:22 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,71 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Tujhe Kitna Chahne Lge Hum",
+    artist: "Arijit Singh, Mithoon",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Shayad",
+    artist: "Pritam, Arijit Singh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Kaun Tujhe",
+    artist: "Palak Muchhal",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Yeh Aaina",
+    artist: "Shreya Ghoshal",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Tum Tak",
+    artist: "A.R. Rahman",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Shoorveer III",
+    artist: "Rapperiya Balam",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Prithvi",
+    artist: "Rapperiya Balam",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Ghamand Kar",
+    artist: "Sachet Tandon, Parampara Tandon",
+    img: "music-8",
+    src: "music-8"
+  },
+
+
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
